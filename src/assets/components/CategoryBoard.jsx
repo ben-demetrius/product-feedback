48d39e7ac6a581f30c7f09b878ff26704a970d9e
ben demetrius <ben.demetrius@liferay.com>
2024-10-21 10:54:25 +0530

    feat: styled categories sidecard.

--- src/assets/components/CategoryBoard.jsx
+++ src/assets/components/CategoryBoard.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import "../styles/app.scss";
 
 const CategoryBoard = () => {
-  const stickerText = "All";
+  const stickerText1 = "All";
+  const stickerText2 = "UI";
+  const stickerText3 = "UX";
+  const stickerText4 = "Enhancement";
+  const stickerText5 = "Bug";
+  const stickerText6 = "Feature";
   return (
     <div className="pfa-categories">
-      <button className="stickers">{stickerText}</button>
+      <button className="stickers">{stickerText1}</button>
+      <button className="stickers">{stickerText2}</button>
+      <button className="stickers">{stickerText3}</button>
+      <button className="stickers">{stickerText4}</button>
+      <button className="stickers">{stickerText5}</button>
+      <button className="stickers">{stickerText6}</button>
     </div>
   );
 };
