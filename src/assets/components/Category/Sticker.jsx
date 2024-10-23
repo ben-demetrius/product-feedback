78fc74fbe883084e13d5f4a4fc95ad693abf3a1d
ben demetrius <ben.demetrius@liferay.com>
2024-10-23 16:50:27 +0530

    feat: managing selection in Sticker.jsx.

--- src/assets/components/Category/Sticker.jsx
+++ src/assets/components/Category/Sticker.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Sticker = ({ stickerText }) => {
+  const [select, setSelect] = useState(false);
+
+  const handleSelect = () => {
+    if (select == false) {
+      setSelect(true);
+    } else {
+      setSelect(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log(`${stickerText}:${select}`);
+  }, [select]);
+
   return (
-    <button className="pfa-stickers pfa-stickers--hover .pfa-stickers--active">
+    <button
+      className={`pfa-stickers pfa-stickers--hover ${
+        select ? `pfa-stickers--active` : ``
+      }`}
+      onClick={handleSelect}
+    >
       {stickerText}
     </button>
   );
