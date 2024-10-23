ae1fa3067fdae85dbe156edc335d10cabf9da332
ben demetrius <ben.demetrius@liferay.com>
2024-10-23 16:18:57 +0530

    refactor: moved the get URL to a variable in CategoryBoard.jsx.

--- src/assets/components/Category/CategoryBoard.jsx
+++ src/assets/components/Category/CategoryBoard.jsx
@@ -3,13 +3,14 @@ import "../../styles/app.scss";
 import { get } from "../../js/httpRequests";
 import Sticker from "./Sticker";
 
+const PicklistURL =
+  "/o/headless-admin-list-type/v1.0/list-type-definitions/by-external-reference-code/categories/list-type-entries";
+
 const CategoryBoard = () => {
   const [picklist, setPicklist] = useState([]);
 
   useEffect(() => {
-    get(
-      "http://localhost:8080/o/headless-admin-list-type/v1.0/list-type-definitions/by-external-reference-code/categories/list-type-entries"
-    ).then((data) => setPicklist(data));
+    get(PicklistURL).then((data) => setPicklist(data));
   }, []);
 
   const list = picklist.map((item, i) => {
--- src/assets/js/httpRequests.js
+++ src/assets/js/httpRequests.js
@@ -1,14 +1,14 @@
-const makeHttpRequest = async ({
-  url = `http://localhost:8080/o/c/feedbackses/`,
-  method,
-  body = null,
-}) => {
+const defaultURL = `/o/c/feedbackses/`;
+const baseURL = `http://localhost:8080`;
+
+const makeHttpRequest = async ({ url = defaultURL, method, body = null }) => {
+  const setUrl = `${baseURL}${url}`;
   const headers = {
     "Content-Type": "application/json; charset=utf8",
     "Access-Control-Allow-Origin": "*",
     Authorization: "Basic " + btoa("test@liferay.com:learn"),
   };
-  return await fetch(url, { headers, method, body });
+  return await fetch(setUrl, { headers, method, body });
 };
 
 const post = async (postData) => {
